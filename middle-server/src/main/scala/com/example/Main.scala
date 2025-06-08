0fe0ab6b1967e1ed1714b63e25cc6f713901b6c3
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-06-08 20:10:10 +0900

    fix: show logs

--- middle-server/src/main/scala/com/example/Main.scala
+++ middle-server/src/main/scala/com/example/Main.scala
@@ -4,31 +4,31 @@ import org.apache.flinkx.api.*
 import org.apache.flinkx.api.serializers.*
 import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction
 
+case class Stock(
+    stock: String,
+    open: Double,
+    high: Double,
+    low: Double,
+    close: Double,
+    timestamp: String
+)
+
 object App {
   def main(args: Array[String]): Unit = {
     // flink stream exec environment
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     // connect socket
     val socketStream = env.socketTextStream("localhost", 5000)
+    env.setParallelism(1)
 
     // logging
+    // stock,open,high,low,close,timestamp
     // Record sample: StockA,309.65,768.04,190.54,415.46,2025-04-25 06:42:20.905995563
     socketStream
-      .map(line => {
-        val parts = line.split(",")
-        if (parts.length == 6) {
-          (
-            parts(0),
-            parts(1).toDouble,
-            parts(2).toDouble,
-            parts(3).toDouble,
-            parts(4).toDouble,
-            parts(5)
-          )
-        } else {
-          throw new IllegalArgumentException(s"Invalid input: $line")
-        }
-      })
+      .map { line =>
+        println(s"RECEIVED: $line")
+        line
+      }
       .print()
 
     env.execute("Start Socket Stream")
